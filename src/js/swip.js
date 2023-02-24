ff65e71403f00773036a15a150b34abd3ebc98db
Yana~ <sharik.spartak2@mail.ru>
2023-02-24 13:32:45 +0300

    add two swip

--- src/js/swip.js
+++ src/js/swip.js
@@ -1,9 +1,12 @@
-const showButton = document.querySelector('.show-more__button')
-const hiddenParts = document.querySelector('.list__hidden')
-const showLess = document.querySelector('.show-more__icon')
+import Swiper from './swiper-bundle.min.js'
 
-let div1 = document.createElement('div')
-let div2 = document.createElement('div')
+export const showButton = document.querySelector('.show-more__button')
+export const hiddenParts = document.querySelector('.list__hidden')
+export const showLess = document.querySelector('.show-more__icon')
+
+export let div1 = document.createElement('div')
+export let div2 = document.createElement('div')
+
 div1.setAttribute('class', 'swiper-slide card')
 div2.setAttribute('class', 'swiper-slide card')
 showButton.addEventListener('click', function () {
@@ -26,9 +29,40 @@ showButton.addEventListener('click', function () {
   }
 })
 
-console.log('4')
-const slider = document.getElementById('swiper')
-let mySwiper
+export const showButton2 = document.querySelector('.show-more__button2')
+export const hiddenParts2 = document.querySelector('.list__hidden2')
+export const showLess2 = document.querySelector('.show-more__icon2')
+
+export let div12 = document.createElement('div')
+export let div22 = document.createElement('div')
+
+div12.setAttribute('class', 'swiper-slide card2')
+div22.setAttribute('class', 'swiper-slide card2')
+showButton2.addEventListener('click', function () {
+  hiddenParts2.classList.toggle('list__hidden2')
+  showLess2.classList.toggle('show-more__icon2')
+  div12.innerHTML = `
+        <img class="card__logo" src="./img/samsung.svg" alt="Samsung">
+        <img src="./img/go.svg" alt="logo" class="card__go">
+        `
+  div22.innerHTML = `
+        <img class="card__logo" src="./img/ViewSonic.svg" alt="ViewSonic">
+        <img src="./img/go.svg" alt="logo" class="card__go">
+        `
+  document.getElementById('slide').appendChild(div12)
+  document.getElementById('slide').appendChild(div22)
+  if (hiddenParts2.classList.contains('list__hidden2')) {
+    showButton2.textContent = 'Показать всё'
+  } else {
+    showButton2.textContent = 'Скрыть'
+  }
+})
+
+export const slider = document.querySelector('.swiper')
+export let mySwiper
+
+export const slider2 = document.querySelector('.swiper2')
+export let mySwiper2
 
 function mobileSlider() {
   if (window.innerWidth <= 767 && slider.dataset.mobile == 'false') {
@@ -46,11 +80,28 @@ function mobileSlider() {
       }
     })
     slider.dataset.mobile = 'true'
+
+    mySwiper2 = new Swiper(slider2, {
+      slidesPerView: 1,
+      spaceBetween: 16,
+      width: 240,
+      height: 72,
+      slideClass: 'card2',
+      pagination: {
+        spaceBetween: 16,
+        loop: true,
+        el: '.swiper-pagination',
+        clickable: true
+      }
+    })
+    slider2.dataset.mobile = 'true'
   }
 
   if (window.innerWidth >= 768) {
     slider.dataset.mobile = 'false'
     mySwiper.destroy()
+    slider2.dataset.mobile = 'false'
+    mySwiper2.destroy()
   }
 }
 
@@ -58,4 +109,3 @@ mobileSlider()
 window.addEventListener('resize', () => {
   mobileSlider()
 })
-console.log('2')
